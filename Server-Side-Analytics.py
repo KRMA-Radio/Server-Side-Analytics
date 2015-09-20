5d677b758b9a4bfc8131075440e87198e564e203
Isaac Park <isaac.a.park@gmail.com>
2015-09-19 20:55:22 -0400

    Print All Access Attempts
    Implements the reading side of Server-Side-Analytics.py

--- Server-Side-Analytics.py
+++ Server-Side-Analytics.py
@@ -3,9 +3,25 @@ import sys
 from Access import Access
 
 
-def LogParser(file, host, tracking_id):
-    f = open(file)
+USAGE = "python Server-Side-Analytics.py [logfile] [hostname] [google analytics tracking id]"
+
+
+def server_side_analytics(f, host: str, tracking_id: str):
     log = Access.from_file(f, host)
 
+    for entry in log:
+        print(entry)
 
-LogParser(sys.argv[1], sys.argv[2], sys.argv[3])
+
+if len(sys.argv) < 4:
+    print("Error: not enough arguments")
+    print("Usage: " + USAGE)
+    exit(-1)
+
+try:
+    file = open(sys.argv[1])
+    server_side_analytics(file, sys.argv[2], sys.argv[3])
+except IOError:
+    print("Error: log file can't be opened")
+    print("Usage: " + USAGE)
+    exit(-2)
